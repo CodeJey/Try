43714acca430b6e81f11a7babaa7d342916d1162
Denny <40717229+CodeJey@users.noreply.github.com>
2021-04-28 23:18:33 +0300

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,6 +1,6 @@
 function loadPage() {
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "cd_catalog.xml", true)
+    xhttp.open("POST", "cd_catalog.xml", true)
     xhttp.send();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
